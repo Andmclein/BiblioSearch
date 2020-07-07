9a78e2f6d2fc60a05b31756fb51e84b43d9c1bff
Andmclein <53324948+Andmclein@users.noreply.github.com>
2020-07-07 16:47:47 -0500

    Se actualiza documentación que falta

---
+++ app/src/main/java/com/poo/bibliosearch/Fragments/LibraryFragmentActivated.java
@@ -0,0 +1,120 @@
+package com.poo.bibliosearch.Fragments;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.poo.bibliosearch.Adapters.AdapterBook;
+import com.poo.bibliosearch.Entities.Book;
+import com.poo.bibliosearch.Entities.Login;
+import com.poo.bibliosearch.R;
+import com.poo.bibliosearch.iComunicaFragments;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import static com.poo.bibliosearch.LoginScreen.BOOKS;
+import static com.poo.bibliosearch.LoginScreen.LOG;
+import static com.poo.bibliosearch.LoginScreen.SHARED_PREFS;
+
+public class LibraryFragmentActivated extends Fragment {
+    //Para acción del botón
+    private onFragmentButtonSelected listener;
+
+    AdapterBook adapterBook;
+    RecyclerView recyclerViewBooks;
+    Button botonCrear;
+    ArrayList<Book> books = new ArrayList<>();
+    ArrayList<Login> logins;
+    SharedPreferences sharedPreferences;
+    Type type;
+    //referencias para comunicar fragments
+    Activity actividad;
+    iComunicaFragments interfaceComunicaFragments;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.library_fragment, container, false);
+        recyclerViewBooks = view.findViewById(R.id.rv_library_list);
+        loadLog();
+        botonCrear = view.findViewById(R.id.btn_newBook);
+
+        botonCrear.setVisibility(View.GONE);
+        recyclerViewBooks.setPadding(0, 0, 0, 0);
+
+        //cargar la lista
+        loadBooks();
+        //mostrar los datos
+        showData();
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+
+        if (context instanceof onFragmentButtonSelected) {
+            listener = (onFragmentButtonSelected) context;
+        }
+
+        super.onAttach(context);
+        sharedPreferences = context.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        if (context instanceof Activity) {
+            this.actividad = (Activity) context;
+            interfaceComunicaFragments = (iComunicaFragments) this.actividad;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    public void loadBooks() {
+        books.clear();
+        books = logins.get(0).getUser().getMyBooks();
+
+    }
+
+    public void loadLog() {
+        Gson gson = new Gson();
+        String itemsJson = sharedPreferences.getString(LOG, null);
+        type = new TypeToken<ArrayList<Login>>() {
+        }.getType();
+        logins = gson.fromJson(itemsJson, type);
+    }
+
+    private void showData() {
+        recyclerViewBooks.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapterBook = new AdapterBook(getContext(), books);
+        recyclerViewBooks.setAdapter(adapterBook);
+
+        adapterBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                interfaceComunicaFragments.sendBooks(books.get(recyclerViewBooks.getChildAdapterPosition(view)));
+            }
+        });
+    }
+
+    public interface onFragmentButtonSelected {
+        public void onButtonSelected();
+
+    }
+
+
+}
